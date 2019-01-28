c98816caa55c585fa52ce6f732787336afd45a9e
Michiel004 <michiel.pieters44@telenet.be>
2019-01-28 22:56:00 +0100

    added comments to lib

--- devlib/nunchuk.c
+++ devlib/nunchuk.c
@@ -4,7 +4,8 @@
 void nunchuk_init()
 {
 	I2C_init();
-	I2C_write(0x52,0x40,0x00); // initialize NUNCHUCK
+	// initialize NUNCHUCK
+	I2C_write(0x52,0x40,0x00); 
 	I2C_write_without_regAddr(0x52, 0x00);  
 }
 
@@ -14,7 +15,8 @@ short nunchuk_button_Z()
 	short  Zbutton;
 	I2C_write(0x52,0x05,0x00); 
 	reg5 = I2C_Read();
-	return Zbutton = reg5 & 0x01;
+	//When the button is pushed the value will be be zero.
+	return Zbutton = reg5 & 0x01; // Return the first bit of reg 5.
 }
 
 short nunchuk_button_C()
@@ -23,7 +25,8 @@ short nunchuk_button_C()
 	short  Cbutton;
 	I2C_write(0x52,0x05,0x00); 
 	reg5 = I2C_Read();
-	return Cbutton = reg5 & 0x02;
+	//When the button is pushed the value will be be zero.
+	return Cbutton = reg5 & 0x02; // Return the second bit of reg 5.
 }
 
 short nunchuk_xValue()
@@ -31,14 +34,14 @@ short nunchuk_xValue()
 	short  reg0;
 	I2C_write(0x52,0x00,0x00); 
 	reg0 = I2C_Read();
-	return reg0 ;
+	return reg0 ; // return the value of reg0 this is the xValue.
 }
 
 short nunchuk_yValue()
 {
 	I2C_write(0x52,0x01,0x00); 
 	short reg1 = I2C_Read();
-	return reg1 ;
+	return reg1 ; // return the value of reg1 this is the yValue.
 }
 
 short nunchuk_xAccell()
@@ -47,8 +50,8 @@ short nunchuk_xAccell()
 	short M = I2C_Read();
 	I2C_write(0x52,0x05,0x00); 
 	short L = I2C_Read();
-	short xAccell =	(M <<2) | ((L & 0x0C) >> 2);
-	return xAccell ;
+	short xAccell =	(M <<2) | ((L & 0x0C) >> 2); // Combine the correct bits of reg2 and reg5. for the xAccell data. 
+	return xAccell ; // Return the 10bit xAccell data. 
 }
 
 short nunchuk_yAccell()
@@ -57,8 +60,8 @@ short nunchuk_yAccell()
 	short M = I2C_Read();
 	I2C_write(0x52,0x05,0x00); 
 	short L = I2C_Read();
-	short yAccell =	(M <<2) | ((L & 0x30) >> 4);
-	return yAccell ;
+	short yAccell =	(M <<2) | ((L & 0x30) >> 4); // Combine the correct bits of reg3 and reg5. for the yAccell data. 
+	return yAccell ; // Return the 10bit yAccell data. 
 }
 
 short nunchuk_zAccell()
@@ -67,7 +70,7 @@ short nunchuk_zAccell()
 	short M = I2C_Read();
 	I2C_write(0x52,0x05,0x00); 
 	short L = I2C_Read();
-	short zAccell =	(M <<2) | ((L & 0xC0) >> 6);
-	return zAccell ;
+	short zAccell =	(M <<2) | ((L & 0xC0) >> 6); // Combine the correct bits of reg4 and reg5. for the zAccell data. 
+	return zAccell ; // Return the 10bit zAccell data.
 }
 
--- devlib/nunchuk.h
+++ devlib/nunchuk.h
@@ -4,12 +4,12 @@
 #include "../lib/rasp3base.h"
 #include <stdint.h>
 
-void  nunchuk_init();
-short nunchuk_button_Z();
-short nunchuk_button_C();
-short nunchuk_xValue();
-short nunchuk_yValue();
-short nunchuk_xAccell();
-short nunchuk_yAccell();
-short nunchuk_zAccell();
+void  nunchuk_init(); // initialize NUNCHUCK
+short nunchuk_button_Z(); // Return the value of the Z button.
+short nunchuk_button_C(); // Return the value of the C button.
+short nunchuk_xValue();	// Return the x coördinaat of the NUNCHUCK.  
+short nunchuk_yValue();	// Return the y coördinaat of the NUNCHUCK. 
+short nunchuk_xAccell();// Return the x accelerometer data of the NUNCHUCK. 
+short nunchuk_yAccell();// Return the y accelerometer data of the NUNCHUCK. 
+short nunchuk_zAccell();// Return the z accelerometer data of the NUNCHUCK. 
 #endif
